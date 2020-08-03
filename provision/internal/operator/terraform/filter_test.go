99d6ef03bf0cd2b5e4392d06998e786fdb83ba5e
Borja Clemente <clebs@users.noreply.github.com>
2020-08-03 14:31:59 +0200

    provision: Cleanup unused variables (#121)
    * Add tfvars filtering
    
    Avoid undeclared vars warnings by filtering out variables per provider.
    
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>
    
    * Add AWS filter even if no support yet
    
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>

---
+++ provision/internal/operator/terraform/filter_test.go
@@ -0,0 +1,41 @@
+package terraform
+
+import (
+	"testing"
+
+	"github.com/kyma-incubator/hydroform/provision/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFilterVars(t *testing.T) {
+	cfg := map[string]interface{}{
+		"project":        "fake-project",
+		"create_timeout": "30m",
+		"update_timeout": "15m",
+		"delete_timeout": "10m",
+		"cluster_name":   "fake-cluster",
+	}
+
+	// filter GCP
+	r := filterVars(cfg, types.GCP)
+	require.Equal(t, cfg, r, "GCP should not filter out any variables")
+
+	// filter Azure
+	expected := map[string]interface{}{
+		"cluster_name": "fake-cluster",
+	}
+	r = filterVars(cfg, types.Azure)
+	require.Equal(t, expected, r, "Azure should filter out variables")
+
+	// filter AWS
+	r = filterVars(cfg, types.AWS)
+	require.Equal(t, cfg, r, "AWS should not filter out any variables")
+
+	// filter Gardener
+	r = filterVars(cfg, types.Gardener)
+	require.Equal(t, cfg, r, "Gardener should not filter out any variables")
+
+	// filter Kind
+	r = filterVars(cfg, types.Kind)
+	require.Equal(t, cfg, r, "Kind should not filter out any variables")
+}
