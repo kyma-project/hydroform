50e5d62b9f0085edf44d7ef5aae331a9a89a5a71
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2020-12-03 15:17:12 +0100

    Review fix

--- parallel-install/pkg/components/doc.go
+++ parallel-install/pkg/components/doc.go
@@ -1,5 +1,5 @@
-//Package components defines installation-related contract for Kyma components.
-//A Component is defined by it's name, namespace, Helm's chart directory in a local filesystem and a set of overrides.
+//Package components defines the installation-related contract for Kyma components.
+//A Component is defined by its name, namespace, Helm's chart directory in a local filesystem and a set of overrides.
 //A properly defined Component can be installed or uninstalled as a Helm release.
 //
 //The code in the package uses user-provided function for logging.
