c39f9c11f2469102c13bfd25050d358b66299bce
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2020-12-03 15:13:56 +0100

    Update parallel-install/pkg/prerequisites/prerequisites.go
    Co-authored-by: Karolina Zydek <karolina.zydek@sap.com>

--- parallel-install/pkg/prerequisites/prerequisites.go
+++ parallel-install/pkg/prerequisites/prerequisites.go
@@ -76,7 +76,7 @@ func InstallPrerequisites(ctx context.Context, kubeClient kubernetes.Interface, 
 //The function supports the Context cancellation.
 //The cancellation is not immediate.
 //If the "cancel" signal appears during the uninstallation step (it's a blocking operation),
-//such cancel condition is detected only after that step is over, and the UninstallPrerequisites returns without an error.
+//such a "cancel" condition is detected only after that step is over, and UninstallPrerequisites returns without an error.
 func UninstallPrerequisites(ctx context.Context, kubeClient kubernetes.Interface, prerequisites []components.Component) <-chan error {
 
 	statusChan := make(chan error)
