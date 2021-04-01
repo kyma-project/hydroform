66877ff944818483961fb55b7f925bc9cc921e3d
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-04-01 17:09:25 +0200

    Use Helm lib to retrieve kyma-meta-labels

--- parallel-install/pkg/helm/metadataprovider.go
+++ parallel-install/pkg/helm/metadataprovider.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
-	"strings"
 	"unicode"
 
 	v1 "k8s.io/api/core/v1"
@@ -14,6 +13,7 @@ import (
 	"github.com/fatih/structs"
 	"helm.sh/helm/v3/pkg/release"
 	"helm.sh/helm/v3/pkg/storage"
+	"helm.sh/helm/v3/pkg/storage/driver"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -51,55 +51,27 @@ func (err *kymaMetadataUnavailableError) Error() string {
 	return fmt.Sprintf("Kyma component metadata not found or incomplete in HELM secret '%s': %s", err.secret, err.err.Error())
 }
 
-//kymaMetadataFieldUnknownError is fired when a field of the metadata object doesn't exist (internal error, should never be fired)
-type kymaMetadataFieldUnknownError struct {
-	field string
-}
-
-func (err *kymaMetadataFieldUnknownError) Error() string {
-	return fmt.Sprintf("Kyma metadata struct does not contain a field '%s'", err.field)
-}
-
 //KymaMetadataProvider enables access to Kyma component metadata and version information
 type KymaMetadataProvider struct {
-	kubeClient kubernetes.Interface
+	kubeClient kubernetes.Interface //TODO: remove this field as soon Helm supports to labels on releases (see)
+	storage    *storage.Storage
 }
 
 //NewKymaMetadataProvider creates a new KymaMetadataProvider
 func NewKymaMetadataProvider(client kubernetes.Interface) *KymaMetadataProvider {
 	return &KymaMetadataProvider{
 		kubeClient: client,
+		storage:    storage.Init(driver.NewSecrets(client.CoreV1().Secrets(""))),
 	}
 }
 
 //Versions returns the set of installed Kyma versions
 func (mp *KymaMetadataProvider) Versions() (*KymaVersionSet, error) {
-	//get all secrets which are labeled as Kyma component
-	compField, err := mp.structField("Component")
+	releases, err := mp.storage.ListDeployed()
 	if err != nil {
 		return nil, err
 	}
-	options := metaV1.ListOptions{
-		LabelSelector: fmt.Sprintf("%s=true", mp.labelName(compField)),
-	}
-	secrets, err := mp.kubeClient.CoreV1().Secrets("").List(context.Background(), options)
-	if err != nil {
-		return nil, err
-	}
-
-	//group secrets by component-name (required to find latest secret)
-	nameField, err := mp.structField("Name")
-	if err != nil {
-		return nil, err
-	}
-	secretsPerComp := make(map[string][]v1.Secret)
-	for _, secret := range secrets.Items {
-		if name, ok := secret.Labels[mp.labelName(nameField)]; ok {
-			secretsPerComp[name] = append(secretsPerComp[name], secret)
-		}
-	}
-
-	versions, err := mp.resolveKymaVersions(secretsPerComp)
+	versions, err := mp.resolveKymaVersions(releases)
 	if err != nil {
 		return nil, err
 	}
@@ -109,14 +81,10 @@ func (mp *KymaMetadataProvider) Versions() (*KymaVersionSet, error) {
 }
 
 //resolveKymaVersions creates KymaVersion instances from Helm Secret labels
-func (mp *KymaMetadataProvider) resolveKymaVersions(secretsPerComp map[string][]v1.Secret) ([]*KymaVersion, error) {
+func (mp *KymaMetadataProvider) resolveKymaVersions(releases []*release.Release) ([]*KymaVersion, error) {
 	versions := make(map[string]*KymaVersion) //we se the opsID as differentiator between the different versions
-	for compName, secrets := range secretsPerComp {
-		latestSecret, err := mp.findLatestSecret(compName, secrets)
-		if err != nil {
-			return nil, err
-		}
-		compMeta, err := mp.unmarshalMetadata(latestSecret)
+	for _, release := range releases {
+		compMeta, err := mp.unmarshalMetadata(release)
 		if err != nil {
 			return nil, err
 		}
@@ -138,15 +106,6 @@ func (mp *KymaMetadataProvider) resolveKymaVersions(secretsPerComp map[string][]
 	return mp.versionFromMap(versions), nil
 }
 
-//structField returns a structField from a KymaComponentMetadata object
-func (mp *KymaMetadataProvider) structField(fieldName string) (*structs.Field, error) {
-	field := structs.New(KymaComponentMetadata{}).Field(fieldName)
-	if field == nil {
-		return nil, &kymaMetadataFieldUnknownError{field: fieldName}
-	}
-	return field, nil
-}
-
 //versionFromMap returns the version instances tracked in the map
 func (mp *KymaMetadataProvider) versionFromMap(versionMap map[string]*KymaVersion) []*KymaVersion {
 	versions := make([]*KymaVersion, 0, len(versionMap))
@@ -154,34 +113,6 @@ func (mp *KymaMetadataProvider) versionFromMap(versionMap map[string]*KymaVersio
 		versions = append(versions, v)
 	}
 	return versions
-}
-
-//findLatestSecret returns the latest Helm secret of a component
-func (mp *KymaMetadataProvider) findLatestSecret(name string, secrets []v1.Secret) (*v1.Secret, error) {
-	var latestSecret v1.Secret
-
-	//find latest Helm secret
-	latestChartVersion := -1
-	secretPrefix := mp.secretPrefix(name)
-	for _, secret := range secrets {
-		if strings.HasPrefix(secret.Name, secretPrefix) {
-			currChartVersion, err := strconv.Atoi(strings.Replace(secret.Name, secretPrefix, "", 1))
-			if err != nil {
-				return nil, &helmSecretNameInvalidError{
-					secret:    secret.Name,
-					namespace: secret.Namespace,
-				}
-			}
-			if currChartVersion > latestChartVersion {
-				latestChartVersion = currChartVersion
-				latestSecret = secret
-			}
-		}
-	}
-	if latestChartVersion == -1 {
-		return nil, &helmReleaseNotFoundError{name: name}
-	}
-	return &latestSecret, nil
 }
 
 //Set adds Kyma metadata labels to a Helm secret
@@ -212,29 +143,11 @@ func (mp *KymaMetadataProvider) Set(release *release.Release, compMetaTpl *KymaC
 
 //Get returns Kyma metadata of an installed component
 func (mp *KymaMetadataProvider) Get(name string) (*KymaComponentMetadata, error) {
-	secret, err := mp.latestSecret(name, "")
+	release, err := mp.storage.Last(name)
 	if err != nil {
 		return nil, err
 	}
-	return mp.unmarshalMetadata(secret)
-}
-
-//latestSecret returns the latest Helm secret of a component
-func (mp *KymaMetadataProvider) latestSecret(name, namespace string) (*v1.Secret, error) {
-	secrets, err := mp.kubeClient.CoreV1().Secrets(namespace).List(context.Background(), metaV1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	latestSecret, err := mp.findLatestSecret(name, secrets.Items)
-	if err != nil {
-		return nil, err
-	}
-	if latestSecret == nil {
-		return nil, &helmReleaseNotFoundError{name: name}
-	}
-
-	return latestSecret, nil
+	return mp.unmarshalMetadata(release)
 }
 
 //marshalMetadata creates a KymaComponentMetadata from secret labels
@@ -258,12 +171,12 @@ func (mp *KymaMetadataProvider) marshalMetadata(secret *v1.Secret, metadata *Kym
 }
 
 //unmarshalMetadata converts a KymaComponentMetadata to secret labels
-func (mp *KymaMetadataProvider) unmarshalMetadata(secret *v1.Secret) (*KymaComponentMetadata, error) {
+func (mp *KymaMetadataProvider) unmarshalMetadata(release *release.Release) (*KymaComponentMetadata, error) {
 	var metadata *KymaComponentMetadata = &KymaComponentMetadata{}
 	var typedValue interface{}
 	var err error
 	for _, field := range structs.New(metadata).Fields() {
-		if value, ok := secret.Labels[mp.labelName(field)]; ok {
+		if value, ok := release.Labels[mp.labelName(field)]; ok {
 			//convert label-values to typed values
 			switch field.Kind() {
 			case reflect.Bool:
@@ -291,7 +204,7 @@ func (mp *KymaMetadataProvider) unmarshalMetadata(secret *v1.Secret) (*KymaCompo
 		}
 	}
 	if err := metadata.isValid(); err != nil {
-		return nil, &kymaMetadataUnavailableError{secret: secret.Name, err: err}
+		return nil, &kymaMetadataUnavailableError{secret: release.Name, err: err}
 	}
 	return metadata, nil
 }
--- parallel-install/pkg/helm/metadataprovider_test.go
+++ parallel-install/pkg/helm/metadataprovider_test.go
@@ -1,18 +1,23 @@
 package helm
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
+
+	"encoding/base64"
 
 	"github.com/stretchr/testify/require"
 	"helm.sh/helm/v3/pkg/release"
-
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 	k8st "k8s.io/client-go/testing"
 )
 
-var expectedLabels = map[string]string{
+var b64 = base64.StdEncoding
+
+var expectedKymaLabels = map[string]string{
 	KymaLabelPrefix + "name":         "test",
 	KymaLabelPrefix + "namespace":    "testNs",
 	KymaLabelPrefix + "component":    "true",
@@ -42,14 +47,43 @@ var kymaCompMetaTpl = &KymaComponentMetadataTemplate{
 	CreationTime: int64(1615831194),
 }
 
+func marshalRelease(rls *release.Release) []byte {
+	b, err := json.Marshal(rls)
+	if err != nil {
+		panic(err)
+	}
+	result := make([]byte, b64.EncodedLen(len(b)))
+	b64.Encode(result, b)
+	return result
+}
+
+func addHelmLabels(labels map[string]string) map[string]string {
+	//copy expected labels
+	result := make(map[string]string, len(expectedKymaLabels)+2)
+	for k, v := range labels {
+		result[k] = v
+	}
+	result["owner"] = "helm"
+	result["name"] = "test"
+	return result
+}
+
 func Test_MetadataGet(t *testing.T) {
 	t.Run("Happy path", func(t *testing.T) {
+		//prepare mock
 		k8sMock := fake.NewSimpleClientset(
 			&v1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test.v1",
 					Namespace: "testNs",
-					Labels:    expectedLabels,
+					Labels:    addHelmLabels(expectedKymaLabels),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test",
+						Version:   1,
+						Namespace: "testNs",
+					}),
 				},
 			},
 		)
@@ -64,38 +98,53 @@ func Test_MetadataGet(t *testing.T) {
 		metaProv := NewKymaMetadataProvider(k8sMock)
 		_, err := metaProv.Get("test")
 		require.Error(t, err)
-		require.Equal(t, err.Error(), (&helmReleaseNotFoundError{name: "test"}).Error())
+		require.Equal(t, "release: not found", err.Error())
 	})
 
 	t.Run("No Metadata found for release release found", func(t *testing.T) {
 		k8sMock := fake.NewSimpleClientset(
 			&v1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:        "sh.helm.release.v1.test.v1",
-					Namespace:   "default",
-					Annotations: map[string]string{"foo": "bar"},
+					Name:      "sh.helm.release.v1.test.v1",
+					Namespace: "default",
+					Labels:    map[string]string{"owner": "helm", "name": "test"},
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test",
+						Version:   1,
+						Namespace: "default",
+					}),
 				},
 			},
 		)
 		metaProv := NewKymaMetadataProvider(k8sMock)
 		_, err := metaProv.Get("test")
 		require.Error(t, err)
-		require.IsType(t, err, (&kymaMetadataUnavailableError{secret: "sh.helm.release.v1.test.v1", err: err}))
+		require.IsType(t, (&kymaMetadataUnavailableError{secret: "sh.helm.release.v1.test.v1", err: err}), err)
 	})
 
 	t.Run("Release version not found", func(t *testing.T) {
 		k8sMock := fake.NewSimpleClientset(
 			&v1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "sh.helm.release.v1.test.vx",
+					Name:      "sh.helm.release.v1.testx.v1",
 					Namespace: "default",
+					Labels:    map[string]string{"owner": "helm", "name": "testx"},
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "testx",
+						Version:   1,
+						Namespace: "default",
+					}),
 				},
 			},
 		)
 		metaProv := NewKymaMetadataProvider(k8sMock)
 		_, err := metaProv.Get("test")
 		require.Error(t, err)
-		require.Equal(t, err.Error(), (&helmSecretNameInvalidError{secret: "sh.helm.release.v1.test.vx", namespace: "default"}).Error())
+		require.Equal(t, "release: not found", err.Error())
 	})
 
 }
@@ -114,7 +163,7 @@ func Test_MetadataSet(t *testing.T) {
 		metaProv := NewKymaMetadataProvider(k8sMock)
 		err := metaProv.Set((&release.Release{Name: "test", Namespace: "testNs", Version: 1}), kymaCompMetaTpl.ForComponents())
 		require.NoError(t, err)
-		require.Equal(t, expectedLabels, k8sMock.Fake.Actions()[1].(k8st.UpdateAction).GetObject().(*v1.Secret).GetObjectMeta().GetLabels())
+		require.Equal(t, expectedKymaLabels, k8sMock.Fake.Actions()[1].(k8st.UpdateAction).GetObject().(*v1.Secret).GetObjectMeta().GetLabels())
 	})
 
 	t.Run("Happy path - for prerequisites", func(t *testing.T) {
@@ -133,14 +182,14 @@ func Test_MetadataSet(t *testing.T) {
 		require.NoError(t, err)
 
 		//align expected values
-		expectedLabelsCopy := make(map[string]string, len(expectedLabels))
-		for k, v := range expectedLabels {
-			expectedLabelsCopy[k] = v
+		expectedKymaLabelsCopy := make(map[string]string, len(expectedKymaLabels))
+		for k, v := range expectedKymaLabels {
+			expectedKymaLabelsCopy[k] = v
 		}
-		expectedLabelsCopy[KymaLabelPrefix+"priority"] = "2"
-		expectedLabelsCopy[KymaLabelPrefix+"prerequisite"] = "true"
+		expectedKymaLabelsCopy[KymaLabelPrefix+"priority"] = "2"
+		expectedKymaLabelsCopy[KymaLabelPrefix+"prerequisite"] = "true"
 
-		require.Equal(t, expectedLabelsCopy, k8sMock.Fake.Actions()[1].(k8st.UpdateAction).GetObject().(*v1.Secret).GetObjectMeta().GetLabels())
+		require.Equal(t, expectedKymaLabelsCopy, k8sMock.Fake.Actions()[1].(k8st.UpdateAction).GetObject().(*v1.Secret).GetObjectMeta().GetLabels())
 	})
 
 	t.Run("Release not found", func(t *testing.T) {
@@ -166,7 +215,17 @@ func Test_Versions(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test.v1",
 					Namespace: "testNs",
-					Labels:    expectedLabels,
+					Labels:    addHelmLabels(expectedKymaLabels),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test",
+						Version:   1,
+						Namespace: "testNs",
+						Info: &release.Info{
+							Status: release.StatusDeployed,
+						},
+					}),
 				},
 			},
 		)
@@ -193,7 +252,7 @@ func Test_Versions(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test.v1",
 					Namespace: "test",
-					Labels: map[string]string{
+					Labels: addHelmLabels(map[string]string{
 						KymaLabelPrefix + "name":         "test",
 						KymaLabelPrefix + "namespace":    "test",
 						KymaLabelPrefix + "component":    "true",
@@ -201,14 +260,24 @@ func Test_Versions(t *testing.T) {
 						KymaLabelPrefix + "version":      "master",
 						KymaLabelPrefix + "operationID":  "aaa",
 						KymaLabelPrefix + "creationTime": "1000000000",
-						KymaLabelPrefix + "priority":     "1"},
+						KymaLabelPrefix + "priority":     "1"}),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test",
+						Version:   1,
+						Namespace: "test",
+						Info: &release.Info{
+							Status: release.StatusDeployed,
+						},
+					}),
 				},
 			},
 			&v1.Secret{ //installed from "master" by operation "aaa:1000000000"
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test2.v2",
 					Namespace: "test2",
-					Labels: map[string]string{
+					Labels: addHelmLabels(map[string]string{
 						KymaLabelPrefix + "name":         "test2",
 						KymaLabelPrefix + "namespace":    "test2",
 						KymaLabelPrefix + "component":    "true",
@@ -216,14 +285,24 @@ func Test_Versions(t *testing.T) {
 						KymaLabelPrefix + "version":      "master",
 						KymaLabelPrefix + "operationID":  "aaa",
 						KymaLabelPrefix + "creationTime": "1000000000",
-						KymaLabelPrefix + "priority":     "2"},
+						KymaLabelPrefix + "priority":     "2"}),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test2",
+						Version:   2,
+						Namespace: "test2",
+						Info: &release.Info{
+							Status: release.StatusDeployed,
+						},
+					}),
 				},
 			},
 			&v1.Secret{ //installed from "2.0.0" release by operation "bbb:2000000000"
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test1.v1",
 					Namespace: "test1",
-					Labels: map[string]string{
+					Labels: addHelmLabels(map[string]string{
 						KymaLabelPrefix + "name":         "test1",
 						KymaLabelPrefix + "namespace":    "test1",
 						KymaLabelPrefix + "component":    "true",
@@ -231,14 +310,24 @@ func Test_Versions(t *testing.T) {
 						KymaLabelPrefix + "version":      "2.0.0",
 						KymaLabelPrefix + "operationID":  "bbb",
 						KymaLabelPrefix + "creationTime": "2000000000",
-						KymaLabelPrefix + "priority":     "1"},
+						KymaLabelPrefix + "priority":     "1"}),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test1",
+						Version:   1,
+						Namespace: "test1",
+						Info: &release.Info{
+							Status: release.StatusSuperseded,
+						},
+					}),
 				},
 			},
 			&v1.Secret{ //installed (upgrade) from "2.0.1" release by operation "ccc:3000000000"
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test1.v2",
 					Namespace: "test1",
-					Labels: map[string]string{
+					Labels: addHelmLabels(map[string]string{
 						KymaLabelPrefix + "name":         "test1",
 						KymaLabelPrefix + "namespace":    "test1",
 						KymaLabelPrefix + "component":    "true",
@@ -246,14 +335,24 @@ func Test_Versions(t *testing.T) {
 						KymaLabelPrefix + "version":      "2.0.1",
 						KymaLabelPrefix + "operationID":  "ccc",
 						KymaLabelPrefix + "creationTime": "3000000000",
-						KymaLabelPrefix + "priority":     "1"},
+						KymaLabelPrefix + "priority":     "1"}),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test1",
+						Version:   2,
+						Namespace: "test1",
+						Info: &release.Info{
+							Status: release.StatusDeployed,
+						},
+					}),
 				},
 			},
 			&v1.Secret{ //installed from "master" by operation "ddd:4000000000"
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "sh.helm.release.v1.test3.v1",
 					Namespace: "test3",
-					Labels: map[string]string{
+					Labels: addHelmLabels(map[string]string{
 						KymaLabelPrefix + "name":         "test3",
 						KymaLabelPrefix + "namespace":    "test3",
 						KymaLabelPrefix + "component":    "true",
@@ -261,7 +360,17 @@ func Test_Versions(t *testing.T) {
 						KymaLabelPrefix + "version":      "master",
 						KymaLabelPrefix + "operationID":  "ddd",
 						KymaLabelPrefix + "creationTime": "4000000000",
-						KymaLabelPrefix + "priority":     "1"},
+						KymaLabelPrefix + "priority":     "1"}),
+				},
+				Data: map[string][]byte{
+					"release": marshalRelease(&release.Release{
+						Name:      "test3",
+						Version:   1,
+						Namespace: "test3",
+						Info: &release.Info{
+							Status: release.StatusDeployed,
+						},
+					}),
 				},
 			},
 		)
@@ -335,8 +444,21 @@ func Test_Versions(t *testing.T) {
 				},
 			},
 		}
+		//compare different versions (distinguished by their operationID)
+		versionMap := make(map[string]*KymaVersion, len(versionSet.Versions))
 		for _, version := range versionSet.Versions {
-			require.Contains(t, expectedVersions, version)
+			versionMap[version.OperationID] = version
+		}
+		for _, kymaVersion := range expectedVersions {
+			version, ok := versionMap[kymaVersion.OperationID]
+			require.True(t, ok, fmt.Sprintf("Version with name is '%s' missing in version set", kymaVersion.Version))
+			require.Equal(t, kymaVersion.Version, version.Version)
+			require.Equal(t, len(kymaVersion.Components), len(version.Components))
+			for _, comp := range kymaVersion.Components {
+				require.Equal(t, comp.Name, comp.Name)
+				require.Equal(t, comp.Priority, comp.Priority)
+				require.Equal(t, comp.CreationTime, comp.CreationTime)
+			}
 		}
 	})
 }
