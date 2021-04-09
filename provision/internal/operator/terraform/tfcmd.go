8107aa4150a6cc9614add5d0b22c7f347a4f8894
Adrian Stobbe <adrian.stobbe@sap.com>
2021-04-09 09:51:44 +0200

    remove bracket

--- provision/internal/operator/terraform/tfcmd.go
+++ provision/internal/operator/terraform/tfcmd.go
@@ -51,7 +51,7 @@ func initArgs(p types.ProviderType, cfg map[string]interface{}, clusterDir strin
 		// TODO remove this condition when fully migrated to modules
 		if m := tfMod(p); m != "" {
 			args = append(args, fmt.Sprintf("-from-module=%s", m))
-		}}
+		}
 	}
 	if runtime.GOOS == "windows" { // remove '\\?\' path prefix
 			clusterDir = clusterDir[4:]
