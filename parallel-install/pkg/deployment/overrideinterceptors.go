2784c31ef2972fd26b750bc123346e2a069d43a3
Patryk Strugacz <patryk.strugacz@sap.com>
2021-04-29 14:16:29 +0200

    Separate file for coreDNS patch, add test

--- parallel-install/pkg/deployment/overrideinterceptors.go
+++ parallel-install/pkg/deployment/overrideinterceptors.go
@@ -1,23 +1,18 @@
 package deployment
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"html/template"
 	"strings"
 	"time"
 
 	"github.com/avast/retry-go"
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/logger"
 	"github.com/pkg/errors"
-	v1 "k8s.io/api/core/v1"
 	apierr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -37,34 +32,6 @@ var retryOptions = []retry.Option{
 	retry.Delay(2 * time.Second),
 	retry.Attempts(3),
 	retry.DelayType(retry.FixedDelay),
-}
-
-var coreDNSPatchTemplate = `
-.:53 {
-    errors
-    health
-    rewrite name regex {{ .DomainName}} istio-ingressgateway.istio-system.svc.cluster.local
-    ready
-    kubernetes cluster.local in-addr.arpa ip6.arpa {
-      pods insecure
-      fallthrough in-addr.arpa ip6.arpa
-    }
-    hosts /etc/coredns/NodeHosts {
-      reload 1s
-      fallthrough
-    }
-    prometheus :9153
-    forward . /etc/resolv.conf
-    cache 30
-    loop
-    reload
-    loadbalance
-}
-`
-
-// CoreDNSPatch contains values to fill template with
-type CoreDNSPatch struct {
-	DomainName string
 }
 
 // OverrideInterceptor is controlling access to override values
@@ -139,7 +106,7 @@ func (i *DomainNameOverrideInterceptor) getDomainName() (domainName string, err 
 		return "", err
 	}
 	if domainName != "" {
-		err = patchCoreDNS(i.kubeClient, `(.*)\.local\.kyma\.devaaaa`, i.log)
+		_, err = patchCoreDNS(i.kubeClient, `(.*)\.local\.kyma\.dev`, i.log)
 		if err != nil {
 			return "", err
 		}
@@ -147,7 +114,7 @@ func (i *DomainNameOverrideInterceptor) getDomainName() (domainName string, err 
 	}
 
 	// In any other environment fallback to a generic remote cluster domain
-	err = patchCoreDNS(i.kubeClient, `(.*)\.kyma\.example\.comeee`, i.log)
+	_, err = patchCoreDNS(i.kubeClient, `(.*)\.kyma\.example\.com`, i.log)
 	if err != nil {
 		return "", err
 	}
@@ -166,75 +133,6 @@ func (i *DomainNameOverrideInterceptor) findLocalDomain() (domainName string, er
 	}
 
 	return "", nil
-}
-
-// patchCoreDNS takes kubeclient and cluster domain as a parameter to patch coredns config
-// e.g. domainName: `(.*)\.local\.kyma\.dev`
-func patchCoreDNS(kubeClient kubernetes.Interface, domainName string, log logger.Interface) (err error) {
-	coreDNSPatch := CoreDNSPatch{DomainName: domainName}
-	patchTemplate := template.Must(template.New("").Parse(coreDNSPatchTemplate))
-	patchBuffer := new(bytes.Buffer)
-	if err = patchTemplate.Execute(patchBuffer, coreDNSPatch); err != nil {
-		return err
-	}
-
-	// TODO: Refactor
-	err = retry.Do(func() error {
-		_, err := kubeClient.AppsV1().Deployments("kube-system").Get(context.TODO(), "coredns", metav1.GetOptions{})
-		if err != nil {
-			if apierr.IsNotFound(err) {
-				log.Info("CoreDNS not found, skipping CoreDNS config patch")
-				return nil
-			}
-			return err
-		}
-
-		configMaps := kubeClient.CoreV1().ConfigMaps("kube-system")
-		coreDNSConfigMap, err := configMaps.Get(context.TODO(), "coredns", metav1.GetOptions{})
-		if err != nil {
-			if apierr.IsNotFound(err) {
-				log.Info("Corefile not found, creating new CoreDNS config")
-				_, err := configMaps.Create(context.TODO(), getNewCoreDNSConfigMap(patchBuffer.String()), metav1.CreateOptions{})
-				if err != nil {
-					log.Warn("Could not create new CoreDNS Corefile config")
-				}
-				return nil
-			}
-			return err
-		}
-
-		if strings.Contains(coreDNSConfigMap.Data["Corefile"], domainName) {
-			log.Info("CoreDNS config already contains proper domain rule")
-			return nil
-		}
-
-		coreDNSConfigMap.Data["Corefile"] = patchBuffer.String()
-		jsontext, err := json.Marshal(coreDNSConfigMap)
-		if err != nil {
-			return err
-		}
-
-		log.Info("Patching CoreDNS config")
-		_, err = configMaps.Patch(context.TODO(), "coredns", types.StrategicMergePatchType, jsontext, metav1.PatchOptions{DryRun: []string{""}})
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}, retryOptions...)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func getNewCoreDNSConfigMap(data string) *v1.ConfigMap {
-	return &v1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{Name: "coredns"},
-		Data:       map[string]string{"Corefile": data},
-	}
 }
 
 // CertificateOverrideInterceptor handles certificates
