14f61fb3ed07dd4cf8d59e1408df630b1c6c19c8
Borja Clemente <clebs@users.noreply.github.com>
2021-06-02 14:41:58 +0200

    Make prerequisites fail fast (#296)
    * Make prerequisites fail fast
    
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>
    
    * Add status logging in fail fast
    
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>

--- parallel-install/pkg/deployment/deployment.go
+++ parallel-install/pkg/deployment/deployment.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/avast/retry-go"
+	"github.com/pkg/errors"
 
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/components"
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/config"
@@ -159,6 +160,13 @@ InstallLoop:
 				i.processUpdateComponent(phase, cmp)
 				//Received a status update
 				if cmp.Status == components.StatusError {
+					// prerequisites fail fast
+					if phase == InstallPreRequisites {
+						err := errors.Wrapf(cmp.Error, "Error deploying prerequisite: %s", cmp.Name)
+						i.processUpdate(phase, ProcessExecutionFailure, err)
+						i.logStatuses(statusMap)
+						return err
+					}
 					errCount++
 				}
 				statusMap[cmp.Name] = cmp.Status
