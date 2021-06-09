d76a23cb652769b5ff11d075009d4eb699f903e6
Jeremy Harisch <jeremy.harisch@sap.com>
2021-06-09 11:39:41 +0200

    Quicksafe

--- parallel-install/pkg/jobmanager/jobs/sampleJob.go
+++ parallel-install/pkg/jobmanager/jobs/sampleJob.go
@@ -1,8 +1,10 @@
-package jobManager
+package jobmanager
 
 import (
 	"context"
 	"fmt"
+
+	"github.com/kyma-incubator/hydroform/parallel-install/pkg/config"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -10,28 +12,32 @@ import (
 
 type job1 struct{}
 
-//var _ = register(job1)
+var _ = register(job1)
 
-
-func (j job1) execute(cfg *config.Config, kubeClient kubernetes.Interface) {
+func (j job1) execute(cfg *config.Config, kubeClient kubernetes.Interface) error {
 	namespace := "kyma-system"
 	pvc := "storage-logging-loki-0"
 
-	pvc, err = clientset.CoreV1().PersistentVolumeClaim(namespace).Get(context.TODO(), pvc, metav1.GetOptions{})
+	pvcReturn, err := kubeClient.CoreV1().PersistentVolumeClaim(namespace).Get(context.TODO(), pvc, metav1.GetOptions{})
+	fmt.Println("#################### PVC##################\n %#v \n %#v", pvcReturn, err)
 
-	if errors.IsNotFound(err) {
-		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-			pod, namespace, statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-	}
-  return nil
+	// if errors.IsNotFound(err) {
+	// 	fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+	// 	fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+	// 		pod, namespace, statusError.ErrStatus.Message)
+	// } else if err != nil {
+	// 	panic(err.Error())
+	// } else {
+	// 	fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	// }
+	return nil
 }
 
-func (j job1) when() {
-	return ("logging", Pre)
+func (j job1) when() (component, executionTime) {
+	return component("logging"), Pre
+}
+
+func (j job1) identify() jobName {
+	return jobName("exampleJob")
 }
