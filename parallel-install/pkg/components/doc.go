733c784275700b34a2dde656d6438177af379785
Dennis Geiselmann <dennis.geiselmann@outlook.de>
2021-10-05 11:48:03 +0200

    remove parallel install module

--- parallel-install/pkg/components/doc.go
+++
@@ -1,6 +0,0 @@
-//Package components defines the installation-related contract for Kyma components.
-//A Component is defined by its name, namespace, Helm's chart directory in a local filesystem, and a set of overrides.
-//A properly defined Component can be installed or uninstalled as a Helm release.
-//
-//The code in the package uses the user-provided function for logging.
-package components
