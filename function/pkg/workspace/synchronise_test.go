db3faa40b08f43e029fb6f5ea680957a7a3ed7a3
MichalKalke <michal.kalke@sap.com>
2023-02-22 15:48:22 +0100

    Fix synch tests finally

--- function/pkg/workspace/synchronise_test.go
+++ function/pkg/workspace/synchronise_test.go
@@ -2,15 +2,16 @@ package workspace
 
 import (
 	"context"
+	"testing"
+
 	"github.com/golang/mock/gomock"
 	"github.com/kyma-project/hydroform/function/pkg/client"
 	mockclient "github.com/kyma-project/hydroform/function/pkg/client/automock"
 	"github.com/kyma-project/hydroform/function/pkg/resources/types"
-	"k8s.io/apimachinery/pkg/api/errors"
+	"github.com/pkg/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"testing"
 )
 
 func Test_Synchronise(t *testing.T) {
@@ -183,139 +184,140 @@ func Test_Synchronise(t *testing.T) {
 			},
 			wantErr: false,
 		},
-		//{
-		//	name:    "getting apirules as unstructured list should fail",
-		//	wantErr: true,
-		//	args: args{
-		//		cfg: Cfg{
-		//			Name:          name,
-		//			Namespace:     namespace,
-		//			SchemaVersion: SchemaVersionV0,
-		//		},
-		//		build: func() client.Build {
-		//
-		//			result := mockclient.NewMockClient(ctrl)
-		//
-		//			result.EXPECT().
-		//				Get(gomock.Any(), name, v1.GetOptions{}).
-		//				Return(&unstructured.Unstructured{Object: map[string]interface{}{"test": "test"}}, nil).
-		//				Times(1)
-		//
-		//			result.EXPECT().
-		//				List(gomock.Any(), v1.ListOptions{}).
-		//				Return(&unstructured.UnstructuredList{}, nil).
-		//				Times(1)
-		//
-		//			result.EXPECT().List(gomock.Any(), v1.ListOptions{}).
-		//				Return(&unstructured.UnstructuredList{}, errors.New("the error")).Times(1)
-		//
-		//			return func(_ string, _ schema.GroupVersionResource) client.Client {
-		//				return result
-		//			}
-		//		}(),
-		//		ctx: context.Background(),
-		//	},
-		//},
-		//{
-		//	name: "inline happy path with subscriptions and apirules",
-		//	args: args{
-		//		cfg: Cfg{
-		//			Name:      name,
-		//			Namespace: namespace,
-		//			Runtime:   types.Nodejs16,
-		//			Source: Source{
-		//				Type: SourceTypeInline,
-		//				SourceInline: SourceInline{
-		//					SourcePath:        "./testdir/inline",
-		//					SourceHandlerName: handlerJs,
-		//					DepsHandlerName:   packageJSON,
-		//				},
-		//			},
-		//			Env: []EnvVar{
-		//				{
-		//					Name:  "TEST_ENV",
-		//					Value: "test",
-		//				},
-		//				{
-		//					Name: "TEST_ENV_SECRET",
-		//					ValueFrom: &EnvVarSource{
-		//						SecretKeyRef: &SecretKeySelector{
-		//							Name: "secretName",
-		//							Key:  "secretKey",
-		//						},
-		//					},
-		//				},
-		//				{
-		//					Name: "TEST_ENV_CM",
-		//					ValueFrom: &EnvVarSource{
-		//						ConfigMapKeyRef: &ConfigMapKeySelector{
-		//							Name: "configMapName",
-		//							Key:  "configMapKey",
-		//						},
-		//					},
-		//				},
-		//			},
-		//			Subscriptions: []Subscription{
-		//				{
-		//					Name: "fixme",
-		//					V0: &SubscriptionV0{
-		//						Protocol: "fixme",
-		//						Filter: Filter{
-		//							Dialect: "fixme",
-		//							Filters: []EventFilter{
-		//								{
-		//									EventSource: EventSource{
-		//										Property: "source",
-		//										Type:     "exact",
-		//										Value:    "the-source",
-		//									},
-		//									EventType: EventType{
-		//										Property: "type",
-		//										Type:     "exact",
-		//										Value:    "t1.v1.0.0",
-		//									},
-		//								},
-		//							},
-		//						},
-		//					},
-		//				},
-		//			},
-		//			APIRules: []APIRule{
-		//				{
-		//					Name:    "test-name",
-		//					Gateway: "test-gateway",
-		//					Service: Service{
-		//						Host: "test-host",
-		//						Port: 9090,
-		//					},
-		//					Rules: []Rule{
-		//						{
-		//							Path:    "test-path",
-		//							Methods: []string{"test-method"},
-		//							AccessStrategies: []AccessStrategie{
-		//								{
-		//									Config: AccessStrategieConfig{
-		//										JwksUrls:       []string{"test-jwks"},
-		//										TrustedIssuers: []string{"test-trusted"},
-		//										RequiredScope:  []string{"test-required"},
-		//									},
-		//									Handler: "test-handler",
-		//								},
-		//							},
-		//						},
-		//					},
-		//				},
-		//			},
-		//		},
-		//		build: func() client.Build {
-		//			c := inlineClient(ctrl, name, namespace)
-		//			return func(_ string, _ schema.GroupVersionResource) client.Client {
-		//				return c
-		//			}
-		//		}(),
-		//	},
-		//	wantErr: false,
-		//},
+		{
+			name:    "getting apirules as unstructured list should fail",
+			wantErr: true,
+			args: args{
+				cfg: Cfg{
+					Name:          name,
+					Namespace:     namespace,
+					SchemaVersion: SchemaVersionV0,
+				},
+				build: func() client.Build {
+
+					result := mockclient.NewMockClient(ctrl)
+
+					result.EXPECT().
+						Get(gomock.Any(), name, v1.GetOptions{}).
+						Return(&unstructured.Unstructured{Object: map[string]interface{}{"test": "test"}}, nil).
+						Times(1)
+
+					result.EXPECT().
+						List(gomock.Any(), v1.ListOptions{}).
+						Return(&unstructured.UnstructuredList{}, nil).
+						Times(1)
+
+					result.EXPECT().List(gomock.Any(), v1.ListOptions{}).
+						Return(&unstructured.UnstructuredList{}, errors.New("the error")).Times(1)
+
+					return func(_ string, _ schema.GroupVersionResource) client.Client {
+						return result
+					}
+				}(),
+				ctx: context.Background(),
+			},
+		},
+		{
+			name: "inline happy path with subscriptions and apirules",
+			args: args{
+				cfg: Cfg{
+					Name:      name,
+					Namespace: namespace,
+					Runtime:   types.Nodejs16,
+					Source: Source{
+						Type: SourceTypeInline,
+						SourceInline: SourceInline{
+							SourcePath:        "./testdir/inline",
+							SourceHandlerName: handlerJs,
+							DepsHandlerName:   packageJSON,
+						},
+					},
+					Env: []EnvVar{
+						{
+							Name:  "TEST_ENV",
+							Value: "test",
+						},
+						{
+							Name: "TEST_ENV_SECRET",
+							ValueFrom: &EnvVarSource{
+								SecretKeyRef: &SecretKeySelector{
+									Name: "secretName",
+									Key:  "secretKey",
+								},
+							},
+						},
+						{
+							Name: "TEST_ENV_CM",
+							ValueFrom: &EnvVarSource{
+								ConfigMapKeyRef: &ConfigMapKeySelector{
+									Name: "configMapName",
+									Key:  "configMapKey",
+								},
+							},
+						},
+					},
+					Subscriptions: []Subscription{
+						{
+							Name: "fixme",
+							V0: &SubscriptionV0{
+								Protocol: "fixme",
+								Filter: Filter{
+									Dialect: "fixme",
+									Filters: []EventFilter{
+										{
+											EventSource: EventSource{
+												Property: "source",
+												Type:     "exact",
+												Value:    "the-source",
+											},
+											EventType: EventType{
+												Property: "type",
+												Type:     "exact",
+												Value:    "t1.v1.0.0",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					APIRules: []APIRule{
+						{
+							Name:    "test-name",
+							Gateway: "test-gateway",
+							Service: Service{
+								Host: "test-host",
+								Port: 9090,
+							},
+							Rules: []Rule{
+								{
+									Path:    "test-path",
+									Methods: []string{"test-method"},
+									AccessStrategies: []AccessStrategie{
+										{
+											Config: AccessStrategieConfig{
+												JwksUrls:       []string{"test-jwks"},
+												TrustedIssuers: []string{"test-trusted"},
+												RequiredScope:  []string{"test-required"},
+											},
+											Handler: "test-handler",
+										},
+									},
+								},
+							},
+						},
+					},
+					SchemaVersion: SchemaVersionV0,
+				},
+				build: func() client.Build {
+					c := inlineClient(ctrl, name, namespace)
+					return func(_ string, _ schema.GroupVersionResource) client.Client {
+						return c
+					}
+				}(),
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
