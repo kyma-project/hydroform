16226c8f32e7eacc906311475adef13bb0757bfd
Damian Badura <damian.badura@sap.com>
2023-06-28 15:05:54 +0200

    remove nodejs14

--- function/pkg/workspace/cfg.go
+++ function/pkg/workspace/cfg.go
@@ -132,7 +132,7 @@ type Cfg struct {
 	Name                 string            `yaml:"name"`
 	Namespace            string            `yaml:"namespace"`
 	Labels               map[string]string `yaml:"labels,omitempty"`
-	Runtime              types.Runtime     `yaml:"runtime" jsonschema:"enum=nodejs14,enum=nodejs16,enum=nodejs18,enum=python39"`
+	Runtime              types.Runtime     `yaml:"runtime" jsonschema:"enum=nodejs16,enum=nodejs18,enum=python39"`
 	RuntimeImageOverride string            `yaml:"runtimeImageOverride,omitempty"`
 	Source               Source            `yaml:"source"`
 	Resources            Resources         `yaml:"resource,omitempty"`
